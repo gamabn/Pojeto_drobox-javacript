3c394f38667137a4981fb3c7eb4055e695a503f5
Flavio Gama <lanbinhohouse@gmail.com>
2024-02-01 10:36:33 -0300

    commit original

---
+++ routes/index.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var router = express.Router();
+var formidable = require('formidable');
+var fs = require('fs');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+  res.render('index', { title: 'Express' });
+
+});
+
+router.delete('/file',(req, res)=>{
+
+  let form = new formidable.IncomingForm({
+    uploadDir: './upload',
+    keepExtensions: true
+  });
+
+  form.parse(req, (err, fields, files) => {
+   // let file = fields.path;
+
+//fs.unlink(fields.path);
+
+ res.json({
+  
+  fields
+ });
+    
+  });
+});
+
+ router.post('/upload', (req, res)=>{
+
+  let form = new formidable.IncomingForm({
+    uploadDir: './upload',
+    keepExtensions: true
+  });
+  form.parse(req, (err,fields,files)=>{
+    res.json({
+      files:files
+    });
+   });
+
+
+ });
+
+module.exports = router;
